5e15ad8cb0c75974895a5c472c8099fac508df11
Jinwoo <zvzv9808@gmail.com>
2024-06-14 21:36:13 +0900

    HSET

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,28 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950,
+		engine: { cylinders: 8 },
+		owner: null || '',
+		service: undefined || ''
+	});
+
+	const car = await client.hGetAll('cars');
+
+	// 만약 리턴할 값을 찾지 못했다면, null을 반환하는 것이 아닌 빈 배열({ })을 return하므로, 아래와 같은 조건문은 활용할 수 없음.
+	// if(!car){
+	//   console.log('Car not found, response with 404');
+	//   return;
+	// }
+
+	if (Object.keys(car).length === 0) {
+		console.log('Car not found, response with 404');
+		return;
+	}
+
+	console.log(car);
+};
 run();
